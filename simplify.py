9e4012ccae67719c97528a1039ac634b030449dc
Jacques Lucke <Jacques@moonicfx.com>
2014-06-18 22:28:33 +0200

    delete all but not finding blocks

--- simplify.py
+++ simplify.py
@@ -22,36 +22,11 @@ def Line(line):
                 i -= 1
             elif line[i] == chr(9): # tab
                 line = line[:i] + line[(i+1):]
-                i -= 1
-            elif line[i] == ";":
-                line = ReplaceIndex(line, "_EE_", i) # for "Expression End"            
+                i -= 1         
             elif line[i] == "{":
                 line = ReplaceIndex(line, "_BS_", i) # for "Block Start"         
             elif line[i] == "}":
                 line = ReplaceIndex(line, "_BE_", i) # for "Block End"
-            elif line[i] == "(":
-                line = ReplaceIndex(line, "_PS_", i) # for "Parameter Start"         
-            elif line[i] == ")":
-                line = ReplaceIndex(line, "_PE_", i) # for "Parameter End"
-            elif line[i] == "=" and line[i-1] != "=" and line[i+1] != "=":
-                line = ReplaceIndex(line, "_AO_", i) # for "Assign Operator"
-        i += 1
-
-    # find string starts and ends
-    i = 0
-    waitingFor = ""
-    while i < len(line):
-        if line[i] == waitingFor:
-            waitingFor = ""
-            line = ReplaceIndex(line, "_SE_", i) # for "String End"
-
-        if waitingFor == "":
-            if line[i] == "'":
-                waitingFor = "'"
-                line = ReplaceIndex(line, "_SS_", i) # for "String Start"
-            if line[i] == '"':
-                waitingFor = '"'
-                line = ReplaceIndex(line, "_SS_", i) # for "String Start"
         i += 1
 
     # remove line breaks
