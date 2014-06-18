20e52e212393007c8d746b7dd0b92725a31b630b
Jacques Lucke <Jacques@moonicfx.com>
2014-06-18 20:53:39 +0200

    find string starts and ends

--- simplify.py
+++ simplify.py
@@ -1,41 +1,54 @@
 def Line(line):
-    newLine = line
-
-    # find expression end, block starts, block ends
+    # find expression end, block starts, block ends; remove whitespaces and tabs
     isString = False
     stringChar = "";
     i = 0
-    while i < len(newLine):
-        if newLine[i] == stringChar:
+    while i < len(line):
+        if line[i] == stringChar:
             isString = False
             stringChar = "";
-        if newLine[i] == "'" and stringChar == "":
+        elif line[i] == "'" and stringChar == "":
             isString = True
             stringChar = "'"
-        if newLine[i] == '"' and stringChar == "":
+        elif line[i] == '"' and stringChar == "":
             isString = True
             stringChar = '"'
             
         if not isString:
-            if newLine[i] == " ":
-                newLine = newLine[:i] + newLine[(i+1):]
+            if line[i] == chr(32): # white space
+                line = line[:i] + line[(i+1):]
                 i -= 1
-            elif newLine[i] == "\t":
-                newLine = newLine[:i] + newLine[(i+1):]
-                i -= 1 
-            elif newLine[i] == ";":
-                newLine = newLine[:i] + "_EE_" + newLine[(i+1):] # for "Expression End"               
-            elif newLine[i] == "{":
-                 newLine = newLine[:i] + "_BS_" + newLine[(i+1):] # for "Block Start"         
-            elif newLine[i] == "}":
-                newLine = newLine[:i] + "_BE_" + newLine[(i+1):] # for "Block End"
-            
+            elif line[i] == chr(9): # tab
+                line = line[:i] + line[(i+1):]
+                i -= 1
+            elif line[i] == ";":
+                line = line[:i] + "_EE_" + line[(i+1):] # for "Expression End"               
+            elif line[i] == "{":
+                 line = line[:i] + "_BS_" + line[(i+1):] # for "Block Start"         
+            elif line[i] == "}":
+                line = line[:i] + "_BE_" + line[(i+1):] # for "Block End"
+        i += 1
+
+    i = 0
+    waitingFor = ""
+    while i < len(line):
+        if line[i] == waitingFor:
+            waitingFor = ""
+            line = line[:i] + "_SE_" + line[(i+1):] # for "String End"
+
+        if waitingFor == "":
+            if line[i] == "'":
+                waitingFor = "'"
+                line = line[:i] + "_SS_" + line[(i+1):] # for "String Start"
+            if line[i] == '"':
+                waitingFor = '"'
+                line = line[:i] + "_SS_" + line[(i+1):] # for "String Start"
         i += 1
 
     # remove line breaks
-    if newLine[-1:] == "\n":
-        newLine = newLine[:-1]
-    return newLine
+    if line[-1:] == "\n":
+        line = line[:-1]
+    return line
 
 def Lines(lines):
     i = 0
